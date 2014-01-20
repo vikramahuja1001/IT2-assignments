445d9e919274a70dcadbf08776bbf1cdfde2d286
Vikram Ahuja <vikram.ahuja@students.iiit.ac.in>
2014-01-20 17:02:49 +0000

    tests.py edited

--- assignments/assignment-1/q1/tests.py
+++ assignments/assignment-1/q1/tests.py
@@ -1,4 +1,4 @@
-from prob1 import *
+from q1 import *
 
 add_friend(1,2)
 add_friend(2,1)
@@ -9,5 +9,16 @@ add_friends(4,(1,2,3))
 remove_user(4)	   
 get_friends(4)
    
-
+remove_user(4)	   
+get_friends(4)
+get_friends_of_friends(1)
+send_message(1,2,"blah")
+send_group_message(1,(2,3,4),"blah_blah")
+get_messages_from_friend(2,1)
+get_messages_from_all_friends(4)
+get_birth_day_messages(3)
+delete_message(1, 2)
+delete_messages(2,(1,2,3,4,5,6))
+delete_all_messages(3)
+delete_messages_from_friend(4,3)
 
